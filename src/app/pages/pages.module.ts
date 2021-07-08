6078884828b322eeb656286fa2035e69cc120f94
Karen Vargas <karencita11981@gmail.com>
2021-07-07 21:37:43 -0500

    seccion6

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from '../app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
-import { AppRoutingModule } from '../app-routing.module';
+
 
 
 
@@ -28,9 +31,12 @@ import { AppRoutingModule } from '../app-routing.module';
       PagesComponent,
     ],
   imports: [
+
+    FormsModule,
     CommonModule,
     SharedModule,
     AppRoutingModule,
+    
   ]
 })
 export class PagesModule { }
